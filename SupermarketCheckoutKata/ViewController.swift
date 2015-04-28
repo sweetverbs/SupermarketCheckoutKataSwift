e3913799e37830da68162a903b8f84331193517f
Matthew Healy <matthew@wetheplural.com>
2015-04-28 22:17:57 +0100

    Initial Commit

---
+++ SupermarketCheckoutKata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SupermarketCheckoutKata
+//
+//  Created by Matthew Healy on 28/04/2015.
+//  Copyright (c) 2015 Matthew Healy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
